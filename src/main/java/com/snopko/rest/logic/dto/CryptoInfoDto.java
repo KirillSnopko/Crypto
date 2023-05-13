60edf564976df85646b35c58dff0d739f0c44c20
Kirill Snopko <kirillsnopko@gmail.com>
2023-05-13 19:35:47 +0300

    included Spring HATEOAS

--- src/main/java/com/snopko/rest/logic/dto/CryptoInfoDto.java
+++ src/main/java/com/snopko/rest/logic/dto/CryptoInfoDto.java
@@ -3,11 +3,12 @@ package com.snopko.rest.logic.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CryptoInfoDto {
+public class CryptoInfoDto extends RepresentationModel<CryptoInfoDto> {
     private int idCrypto;
     private double price;
 }
--- src/main/java/com/snopko/rest/logic/dto/UserDtoResponce.java
+++ src/main/java/com/snopko/rest/logic/dto/UserDtoResponce.java
@@ -3,11 +3,12 @@ package com.snopko.rest.logic.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDtoResponce {
+public class UserDtoResponce extends RepresentationModel<UserDtoResponce> {
     private long id;
     private int idCrypto;
     private String symbol;
